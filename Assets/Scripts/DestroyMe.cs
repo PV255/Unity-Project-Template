3938c6e80bf3bd64706cdfdb22ffe5ac2952fb9b
Gleren37 <glerren37@gmail.com>
2014-11-16 18:53:08 +0100

    Food bug fixed

--- Assets/Scripts/DestroyMe.cs
+++ Assets/Scripts/DestroyMe.cs
@@ -7,6 +7,8 @@ public class DestroyMe : MonoBehaviour
 	public GameObject snakeprefab;
 	public GameObject head;
 	public GameObject parent;
+	private bool alive = true;
+
 	void Start()
 	{
 		head = GameObject.Find ("Snake");
@@ -14,24 +16,28 @@ public class DestroyMe : MonoBehaviour
 	// Use this for initialization
 	void OnTriggerEnter(Collider c)
 	{
-		print ("colliding");
-		Destroy (this.gameObject);
+		if (alive) 
+		{
+			alive = false;
+			print ("colliding");
+			Destroy (this.gameObject);
 
+			float px = (float)Random.value * 10 - 0.5f;
+			float py = (float)Random.value * 10 - 9.5f;
+			float pz = (float)Random.value * 10 - 4.5f;
+			Vector3 foodPosition = new Vector3 (px, py, pz);
+			Instantiate (food, foodPosition, Quaternion.identity);
+			print ("created food");
+			//tu bude timer
+			//Vector3 NewBodyPosition = new Vector3 (c.transform.position.x, c.transform.position.y-1, c.transform.position.z);
+			WhereIAM pos = head.GetComponent <WhereIAM> ();
+			Vector3 NewBodyPosition = pos.returnPreviousPosition ();
+			GameObject newbody = (GameObject)Instantiate (snakeprefab, NewBodyPosition, Quaternion.identity);
+			print ("created body");
+			newbody.transform.parent = FindSon (head.transform);
+			print ("created body with parent " + newbody.transform.parent);
+		}
 
-		float px = (float) Random.value*10-0.5f;
-		float py = (float) Random.value*10-9.5f;
-		float pz = (float) Random.value*10-4.5f;
-		Vector3 foodPosition = new Vector3(px, py, pz);
-		Instantiate(food, foodPosition, Quaternion.identity);
-		print ("created food");
-		//tu bude timer
-		//Vector3 NewBodyPosition = new Vector3 (c.transform.position.x, c.transform.position.y-1, c.transform.position.z);
-		WhereIAM pos = head.GetComponent <WhereIAM> ();
-		Vector3 NewBodyPosition = pos.returnPreviousPosition ();
-		GameObject newbody = (GameObject) Instantiate (snakeprefab, NewBodyPosition, Quaternion.identity);
-		print ("created body");
-		newbody.transform.parent = FindSon(head.transform);
-		print ("created body with parent " + newbody.transform.parent);
 	}
 	public Transform FindSon(Transform oldParent)
 	{

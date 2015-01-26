8a17e61fbd0adb66f748ae211835dbf708e0e51b
kaqpa <veghova.gabriela@gmail.com>
2015-01-26 18:29:44 +0100

    Rychlost

--- Assets/Scripts/ScoreLoadAfterSnakeDeath.cs
+++ Assets/Scripts/ScoreLoadAfterSnakeDeath.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class ScoreLoadAfterSnakeDeath : MonoBehaviour {
 
 	void OnDestroy(){
-		Time.timeScale = 0.1F;
+		//Time.timeScale = 0.1F;
+		print ("spomalenie");
 		GameObject.Find ("_GameManager_").GetComponent<GameManager> ().TakeOffPointsAddedAfterSnakeDead (3);
 		//yield return new WaitForSeconds(2);
 

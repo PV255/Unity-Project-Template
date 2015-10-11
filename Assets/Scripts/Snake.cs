f99ec38b4631868f4178741883e0bc39c6d7f5aa
Michal Stora <stora.michal@gmail.com>
2015-10-11 19:43:03 +0200

    Small changes in behaviour

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -5,6 +5,13 @@ using System.Linq;
 
 public class Snake : MonoBehaviour {
 
+    public GameObject foodPrefab;
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
     public float moveTime;
     public float moveRate;
     public float moveDistance;
@@ -17,6 +24,7 @@ public class Snake : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("Move", moveTime, moveRate);
+        SpawnFood();
 	}
 	
 	// Update is called once per frame
@@ -57,9 +65,10 @@ public class Snake : MonoBehaviour {
 
         transform.Translate(dir * moveDistance);
         if (ate) {
-
+            SpawnFood();
             tail.Insert(0, ((GameObject)Instantiate(tailPrefab, currentPossition, Quaternion.identity)).transform);
             ate = false;
+            
         }
         else if (tail.Count > 0) {
             tail.Last().position = currentPossition;
@@ -69,5 +78,21 @@ public class Snake : MonoBehaviour {
                 
            
         }
+    }
+
+    void SpawnFood()
+    {
+        // x position between left & right border
+        int x = (int)Random.Range(borderLeft.position.x,
+                                  borderRight.position.x);
+
+        // y position between top & bottom border
+        int y = (int)Random.Range(borderBottom.position.y,
+                                  borderTop.position.y);
+
+        // Instantiate the food at (x, y)
+        Instantiate(foodPrefab,
+                    new Vector2(x, y),
+                    Quaternion.identity); // default rotation
     }
 }

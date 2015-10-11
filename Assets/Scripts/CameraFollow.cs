182af52517893bebf3fca4bd4c4945a28b4fd8be
Martin Stefanko <mjolnir122@gmail.com>
2015-10-11 17:12:00 +0200

    basic scoring system

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,23 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
 
     public GameObject target;
     public float height;
     public float distance;
 
-	// Use this for initialization
-	void Start () {
-        transform.position = new Vector3(target.transform.position.x, 
-                                        target.transform.position.y + height,
-                                        target.transform.position.z);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        ResetCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector3 temp = transform.position;
-        temp.z = target.transform.position.z - distance;
+        Vector3 playerPos = target.transform.position;
+
+        temp.z = playerPos.z - distance;
         transform.position = temp;
-	}
+    }
+
+    public void ResetCamera()
+    {
+        transform.position = new Vector3(target.transform.position.x,
+                                        target.transform.position.y + height,
+                                        target.transform.position.z - distance);
+    }
 }
--- Assets/Scripts/PlayerLimit.cs
+++ Assets/Scripts/PlayerLimit.cs
@@ -1,24 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerLimit : MonoBehaviour
 {
+    public Text scoreText;
 
     private Vector3 startPos;
+    private GameObject mainCamera;
+    private int score;
+   
 
     // Use this for initialization
     void Start()
     {
+        mainCamera = GameObject.Find("Main Camera");
         startPos = transform.position;
+        score = 0;
+        SetScoreText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.position.y);
         if (transform.position.y < -5)
         {
-            transform.position = startPos;
+            ResetLevel();
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Points"))
+        {
+            collision.gameObject.SetActive(false);
+            score += 1;
+            SetScoreText();
+        }
+
+    }
+
+    private void ResetLevel()
+    {
+        transform.position = startPos;
+        mainCamera.GetComponent<CameraFollow>().ResetCamera();
+    }
+
+    private void SetScoreText()
+    {
+        scoreText.text = "Score: " + score; 
     }
 }

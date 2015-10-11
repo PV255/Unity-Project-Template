05612a1757fabeec108cee573708e7ee7ef36af2
Michal Stora <stora.michal@gmail.com>
2015-10-11 17:38:27 +0200

    Initial commit

---
+++ Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour {
+
+    public GameObject foodPrefab;
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	
+}

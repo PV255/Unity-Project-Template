10daa031b497e1a252cc82cf9fbe5ac681276cf5
Michal Stora <stora.michal@gmail.com>
2015-10-11 18:36:56 +0200

    Moving snake + snake prefab

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snake : MonoBehaviour {
+
+    public float moveTime;
+    public float moveRate;
+    public float moveDistance;
+    Vector2 dir = Vector2.right;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Move", moveTime, moveRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Move() {
+        //TODO move
+        transform.Translate(dir * moveDistance);
+    }
+}
--- Assets/Scripts/SpawnFood.cs
+++ Assets/Scripts/SpawnFood.cs
@@ -12,8 +12,24 @@ public class SpawnFood : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-	
-	}
-	
-	
+        InvokeRepeating("Spawn", 3, 4); 
+    }
+    
+    // Spawn one piece of food
+    void Spawn()
+    {
+        // x position between left & right border
+        int x = (int)Random.Range(borderLeft.position.x,
+                                  borderRight.position.x);
+
+        // y position between top & bottom border
+        int y = (int)Random.Range(borderBottom.position.y,
+                                  borderTop.position.y);
+
+        // Instantiate the food at (x, y)
+        Instantiate(foodPrefab,
+                    new Vector2(x, y),
+                    Quaternion.identity); // default rotation
+    }
+
 }

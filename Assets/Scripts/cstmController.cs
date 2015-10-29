199a673de3254e415ca04a25353105fd86ce5d89
Ľubor Slušný <lubor.slusny@gmail.com>
2015-10-29 21:08:28 +0100

    custom controller: slightly updated jumping, added camera based controls and nodes for camera rotation(all wip)

--- Assets/Scripts/cstmController.cs
+++ Assets/Scripts/cstmController.cs
@@ -5,15 +5,20 @@ using UnityStandardAssets.CrossPlatformInput;
 public class cstmController : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
-    float gravity;
-    float nextJump;
+    GameObject camera;
+    public float gravity;
+    public float nextJump;
+
+    bool grounded;
 
     // Use this for initialization
     void Start()
     {
+        camera = GameObject.Find("cstmCamera");
         m_Rigidbody = GetComponent<Rigidbody>();
         gravity = 10;
         nextJump = Time.time;
+        grounded = false;
     }
 
     // Update is called once per frame
@@ -23,25 +28,35 @@ public class cstmController : MonoBehaviour
             
     }
 
+    void OnCollisionStay(Collision c)
+    {
+        foreach(ContactPoint contact in c.contacts) {
+            if (contact.point.y < transform.position.y)
+            {
+                grounded = true;
+                break;
+            }
+        }
+    }
+
     private void FixedUpdate()
     {
         float h = CrossPlatformInputManager.GetAxis("Horizontal");
         float v = CrossPlatformInputManager.GetAxis("Vertical");
 
-        m_Rigidbody.velocity = (v * Vector3.forward + h * Vector3.right) * 7;
+        Transform dir = camera.transform;
 
-        if (CrossPlatformInputManager.GetButtonDown("Jump") && Time.time > nextJump)
+        //m_Rigidbody.velocity = (v * Vector3.forward + h * Vector3.right) * 10;
+        m_Rigidbody.velocity = (v * dir.forward + h * dir.right) * 10;
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && grounded)
         {
-            // m_Rigidbody.velocity += Vector3.up * 20f;
-            // m_Rigidbody.AddForce(Vector3.up * 10);
-            transform.position += Vector3.up * 3;
-            nextJump = Time.time + 0.3f;
+            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, 0, m_Rigidbody.velocity.z);
+            m_Rigidbody.AddForce(new Vector3(0f, 100, 0f), ForceMode.VelocityChange);
+            //grounded = false;
         }
 
-        if (Time.time > nextJump)
-        {
-            m_Rigidbody.velocity += Vector3.down * gravity;
-        }
-
+        m_Rigidbody.AddForce(new Vector3(0f, -500, 0f), ForceMode.Acceleration);    
     }
+
 }

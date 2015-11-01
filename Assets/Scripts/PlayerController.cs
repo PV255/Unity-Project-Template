3342add9dde20e23e9b7c7b7859dc23ab514b88b
xstefank <mjolnir122@gmail.com>
2015-11-01 09:36:46 +0100

    cstmController -> PlayerController, code revision

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody m_Rigidbody;
+    GameObject camera;
+    private float gravity;
+
+    bool is_grounded;
+    bool is_jumping;
+
+    public float maxJump;
+    public float speed;
+
+    private float currentMaxY;
+
+    void Start()
+    {
+        camera = GameObject.Find("cstmCamera");
+        m_Rigidbody = GetComponent<Rigidbody>();
+        gravity = 3.7f;
+
+        is_jumping = false;
+    }
+
+    void Update()
+    {
+        if (is_grounded)
+        {
+            if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            {
+                is_grounded = false;
+                is_jumping = true;
+                currentMaxY = m_Rigidbody.transform.position.y + maxJump;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+
+        m_Rigidbody.velocity = (v * Vector3.forward + h * Vector3.right) * speed;
+
+        /*Transform dir = camera.transform;
+ 
+        //m_Rigidbody.velocity = (v * Vector3.forward + h * Vector3.right) * 10;
+        m_Rigidbody.velocity = (v * dir.forward + h * dir.right) * 10;*/
+
+        if (is_jumping)
+        {
+            transform.position += Vector3.up * 0.16f;
+
+            if (transform.position.y > currentMaxY) is_jumping = false;
+        }
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, 0.3f))
+        {
+            is_grounded = true;
+        }
+
+
+        m_Rigidbody.velocity += Vector3.down * gravity;
+    }
+}

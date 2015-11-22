4072b6caf42805e9fe667975038c69332d7aa0b8
xstefank <mjolnir122@gmail.com>
2015-11-22 18:10:19 +0100

    fall rock 01, collider and fall

---
+++ Assets/Scripts/FallRockScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallRockScript : MonoBehaviour {
+
+    public GameObject rock;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collision");
+        if (other.CompareTag("Player"))
+        {
+            rock.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}

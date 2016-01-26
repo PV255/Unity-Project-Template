a1bbeb941b8e719f5a4ba491bcf3478eecbebdc5
Ľubor Slušný <lubor.slusny@gmail.com>
2016-01-26 14:30:02 +0100

    updated main menu

--- Assets/Scripts/MenuBehaviourScript.cs
+++ Assets/Scripts/MenuBehaviourScript.cs
@@ -3,9 +3,19 @@ using System.Collections;
 
 public class MenuBehaviourScript : MonoBehaviour {
     public void newGame(){
+        if (GameManager.Instance != null) {
+            for (int i = 0; i < 5; i++) {
+                GameManager.Instance.AddLife();
+            }
+        }
+
         Application.LoadLevel(1);
     }
-    
+
+    public void gameOverLeave(){
+        Application.LoadLevel(0);
+    }
+
     public void quitGame(){
         Application.Quit();
     }

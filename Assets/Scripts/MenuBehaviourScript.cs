0b20392070739554b1080bc73c9bc9684582b6eb
Ľubor Slušný <lubor.slusny@gmail.com>
2016-01-26 17:15:52 +0100

    updated main menu scene, fixed game over/return to main menu button

--- Assets/Scripts/MenuBehaviourScript.cs
+++ Assets/Scripts/MenuBehaviourScript.cs
@@ -2,21 +2,28 @@
 using System.Collections;
 
 public class MenuBehaviourScript : MonoBehaviour {
-    public void newGame(){
-        if (GameManager.Instance != null) {
-            for (int i = 0; i < 5; i++) {
-                GameManager.Instance.AddLife();
-            }
-        }
+    public Transform camera = null;
 
+    public void newGame(){
         Application.LoadLevel(1);
+        GameManager.Instance.newGameStarted();
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    Vector3 pos = new Vector3(0, 0, 0);
+    Vector3 y = new Vector3(0, 1, 0);
+
+    void Update() {
+        if (camera != null) {
+            camera.RotateAround(pos, y, Time.deltaTime);
+        }
     }
 
     public void gameOverLeave(){
         Application.LoadLevel(0);
-    }
-
-    public void quitGame(){
-        Application.Quit();
     }
 }

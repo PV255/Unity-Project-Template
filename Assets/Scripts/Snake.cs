bd9fe58e3b827292ba847cfacf04923dc5cd2a7e
Michal Stora <stora.michal@gmail.com>
2016-01-26 16:16:36 +0100

    food won't be genrated to the corners from now on

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -20,7 +20,7 @@ public class Snake : MonoBehaviour {
 
     public float distance;
     public float moveTime;
-    public float moveRate;
+    public float moveChangeRate;
     public float moveDistance;
     public GameObject tailPrefab;
     public GameObject newPortal;
@@ -32,18 +32,31 @@ public class Snake : MonoBehaviour {
     bool shrink = false;
     bool portal = false;
     private int fixedUpdateCounter = 0;
+
+    private static int numOfRows = 12;
+    private static int numOfColls = 20; 
+
+
+    bool[][] obstacle = new bool[numOfRows][] ; /*12*20*/
 	// Use this for initialization
 	void Start () {
         background = GameObject.FindGameObjectWithTag("Background");
         AddPortalSript = background.GetComponent<AddPortal>();
         fixedUpdateCounter = 0;
+        for (int i = 0; i < numOfRows; i++)
+        {
+            obstacle[i] = new bool[numOfColls];
+        }
+        forgetObstacles();
         SpawnFood();
+        
 	}
 
+    /*guarantees same time between each update*/ 
     void FixedUpdate() {
         fixedUpdateCounter++;
         float time = (float)moveTime / 0.02f;
-        Debug.Log("Fix update: " + time +" counter: "+ fixedUpdateCounter);
+        //Debug.Log("Fix update: " + time +" counter: "+ fixedUpdateCounter);
         if ((int)time == fixedUpdateCounter) {
             fixedUpdateCounter = 0;
             Move();
@@ -76,7 +89,7 @@ public class Snake : MonoBehaviour {
             // Remove the Food
             Destroy(coll.gameObject);
         }
-        if (coll.name.StartsWith(decreaseSnakeLengthFoodPrefab.name))
+        else if (coll.name.StartsWith(decreaseSnakeLengthFoodPrefab.name))
         {
             // Get shorter in next Move call
             shrink = true;
@@ -84,16 +97,33 @@ public class Snake : MonoBehaviour {
             // Remove the Food
             Destroy(coll.gameObject);
         }
+        else if (coll.name.StartsWith(incraseSnakeSpeedFoodPrefab.name))
+        {
+            //increase speed of snake ??do we increase the lenght of snake as well??
+            UpdateSpeed(true);
+            SpawnFood();
+            Destroy(coll.gameObject);
+        }
+        else if (coll.name.StartsWith(decreaseSnakeSpeedPrefab.name))
+        {
+            //decrease speed
+            UpdateSpeed(false);
+            SpawnFood();
+            Destroy(coll.gameObject);
+        }
         // Collided with Tail or Border
-        else if(coll.name.StartsWith("PortalPrefab")){
-             // = GetComponent(AddPortal); 
+        else if (coll.name.StartsWith("PortalPrefab"))
+        {
+            // = GetComponent(AddPortal); 
             List<AddPortal.Tuple> portals = AddPortalSript.portals;
             PortalId something = coll.gameObject.GetComponent<PortalId>();
             int id = something.id;
-            Debug.Log("Collision portal id: "+id);
+            Debug.Log("Collision portal id: " + id);
             AddPortal.Tuple onIndex = null;
-            foreach (AddPortal.Tuple tup in portals){
-                if (tup.outputPortal.getId() == id) {
+            foreach (AddPortal.Tuple tup in portals)
+            {
+                if (tup.outputPortal.getId() == id)
+                {
                     onIndex = tup;
                     break;
                 }
@@ -101,7 +131,7 @@ public class Snake : MonoBehaviour {
             if (onIndex != null)
             {
 
-                Vector3 head = new Vector3(dir.x,dir.y,0);
+                Vector3 head = new Vector3(dir.x, dir.y, 0);
                 Vector3 moveDirection = head;
                 //transform.rotation = Quaternion.identity;
                 if (moveDirection != Vector3.zero)
@@ -113,16 +143,18 @@ public class Snake : MonoBehaviour {
                     }
                     else
                     {
-                        angle =Mathf.RoundToInt(angle)  / 90 * 90;
+                        angle = Mathf.RoundToInt(angle) / 90 * 90;
                     }
                     transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
                 }
                 dir = onIndex.outputPortal.getHeading();
                 //transform.position = new Vector2(0,0);
-                if (dir == Vector2.left) {
+                if (dir == Vector2.left)
+                {
                     transform.rotation = new Quaternion(0, 0, 0, 1);
                 }
-                if (dir == Vector2.right) {
+                if (dir == Vector2.right)
+                {
                     transform.rotation = new Quaternion(0, 1, 0, 0);
                 }
                 if (dir == Vector2.down)
@@ -138,7 +170,7 @@ public class Snake : MonoBehaviour {
                 //transform.rotation = Quaternion.LookRotation(new Vector3(0,dir.x,dir.y));
                 //transform.LookAt(onIndex.outputPortal.getHeading());
                 transform.position = onIndex.outputPortal.getPosition();
-                
+
             }
 
         }
@@ -151,12 +183,13 @@ public class Snake : MonoBehaviour {
         {
             // ToDo 'You lose' screen
             Debug.Log("collision with: " + coll.name);
-            Debug.Log("Score: " +tail.Count);
-            foreach(Transform ta in tail){
+            Debug.Log("Score: " + tail.Count);
+            foreach (Transform ta in tail)
+            {
                 Destroy(ta.gameObject);
             }
             tail = new List<Transform>();
-            
+
             transform.position = new Vector2(-7, 1);
         }
     }
@@ -182,6 +215,7 @@ public class Snake : MonoBehaviour {
                 tail.Insert(0, tail.Last());
                 tail.RemoveAt(tail.Count - 1);
                 tail.RemoveAt(tail.Count - 1);
+                SpawnFood();
             }
             else {
                 /*GAME OVER dlzka hada je 0*/
@@ -202,28 +236,95 @@ public class Snake : MonoBehaviour {
         return false;
     }
 
-
+    void UpdateSpeed(bool increase) {
+        if (increase) {
+            moveTime -= moveChangeRate;
+            
+        } else {
+            moveTime += moveChangeRate;
+        }
+        fixedUpdateCounter = 0;
+    }
 
     void SpawnFood()
     {
-        // x position between left & right border
-        int x = (int)Random.Range(borderLeft.position.x,
-                                  borderRight.position.x-2);
-
-        // y position between top & bottom border
-        int y = (int)Random.Range(borderBottom.position.y-2,
-                                  borderTop.position.y);
         Vector2 pos;
-        pos.x = x;
-        pos.y = y;
-        if (pos.x > 0) pos.x = Mathf.Round((int)pos.x / 2) * 2 + 1;
-        if (pos.x < 0) pos.x = Mathf.Round((int)pos.x / 2) * 2 - 1;
-        if (pos.y < 0) pos.y = Mathf.Round((int)pos.y / 2) * 2 - 1;
-        if (pos.y > 0) pos.y = Mathf.Round((int)pos.y / 2) * 2 + 1;
-        Debug.Log("Position food" + pos);
+        int foodX, foodY;
+        do
+        {
+            // x position between left & right border
+            int x = (int)Random.Range(borderLeft.position.x,
+                                      borderRight.position.x + 1);
+
+            // y position between top & bottom border
+            int y = (int)Random.Range(borderBottom.position.y + 1,
+                                      borderTop.position.y);
+            
+            pos.x = x;
+            pos.y = y;
+            x = Mathf.Abs((int)(borderRight.position.x - borderLeft.position.x));
+            y = Mathf.Abs((int)(borderBottom.position.y - borderTop.position.y));
+            //Debug.Log("rozdiely na borderoch, x: " + x + " y: " + y);
+            if (pos.x > 0) pos.x = Mathf.Round((int)pos.x / 2) * 2 - 1;
+            if (pos.x <= 0) pos.x = Mathf.Round((int)pos.x / 2) * 2 + 1;
+            if (pos.y <= 0) pos.y = Mathf.Round((int)pos.y / 2) * 2 + 1;
+            if (pos.y > 0) pos.y = Mathf.Round((int)pos.y / 2) * 2 - 1;
+            //while popoziciach je na mieste x a y sa teraz nachadza rozpetie medzi hranicami
+            //foodX = (int)Mathf.Round(pos.x) / 2 + numOfRows / 2 ;
+            foodY = whichInRange((int)Mathf.Round(borderBottom.position.y),(int)Mathf.Round(pos.y), y / numOfRows);
+            foodX = whichInRange((int)Mathf.Round(borderLeft.position.x), (int)Mathf.Round(pos.x), x / numOfColls);
+            Debug.Log("suradnice do pola prekazok x: "+foodX+ " y: " + (numOfRows - foodY - 1));
+        } while (!(isSafeFoodPlant(foodX, numOfRows - foodY - 1)));
+        Debug.Log("Position food: " + pos);
         // Instantiate the food at (x, y)
+        //need to shift the number
         Instantiate(foodPrefab,
                     new Vector2(pos.x, pos.y),
                     Quaternion.identity); // default rotation
+    }
+
+    int whichInRange(int start, int position, int step) {
+        Debug.Log("start: " + start + " position: " + position + " step: " + step);
+        int current = start + step;
+        int i = 0;
+        while (current <= position)
+        {
+            current += step;
+            i++;
+        }
+        return i;
+    }
+
+    /*in case there are obstacles on board, we need to check whether the food is not going to be planted in some corner, since the snake can't rotate it's head*/
+    bool isSafeFoodPlant(int x, int y) {
+        if (((x == 0) && (y == 0)) || ((x == numOfRows) && (y == 0)) ||
+            ((x == numOfRows) && (y == numOfColls)) || ((x == 0) && (y == numOfColls)))
+        {
+            return false; //corners of game plan
+        }
+        else if (obstacle[y][x] == true) {
+            return false; //hit with the obstacle
+        }
+        else if ((y == 0) || (y == numOfColls))
+        {
+            if ((obstacle[y][x - 1]) || obstacle[y][x + 1]) { return false; } // food by vertical border
+        }
+        else if ((x == 0) || (x == numOfRows)) {
+            if ((obstacle[y - 1][x]) || (obstacle[y + 1][x])) { return false; } // food by horizontal border
+        }
+        else if ((obstacle[y-1][x] && obstacle[y][x-1]) || (obstacle[y - 1][x] && obstacle[y][x + 1]) || 
+            (obstacle[y][x - 1] && obstacle[y + 1][x]) || (obstacle[y ][x-1] && obstacle[y + 1][x + 1]))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    void forgetObstacles() {
+        for (int i = 0; i < numOfRows; i++) {
+            for (int j = 0; j < numOfColls; j++) {
+                obstacle[i][j] = false;
+            }
+        }
     }
 }

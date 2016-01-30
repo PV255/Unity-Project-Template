0352d21971f6a3f7b96ef72ea0f98f78630e210d
xstefank <mjolnir122@gmail.com>
2016-01-30 16:04:54 +0100

    earth lvl trap death fix

--- Assets/Scripts/Traps/FallRock01Collider.cs
+++ Assets/Scripts/Traps/FallRock01Collider.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class FallRock01Collider : MonoBehaviour {
 
-    private GameManager gameManager;
+    private bool took_life;
 
     // Use this for initialization
     void Start () {
-        gameManager = GameManager.Instance;
+        took_life = false;
     }
 
     // Update is called once per frame
@@ -18,9 +18,10 @@ public class FallRock01Collider : MonoBehaviour {
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision");
-        if (collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("Player") && !took_life)
         {
-            gameManager.GetComponent<GameManager>().DestroyLife();
+            collision.collider.GetComponent<PlayerController>().killPlayer();
+            took_life = true;
         }
     }
 }

b8563c4594ebb8388983d6d163df5d54fc0d8e85
xstefank <mjolnir122@gmail.com>
2016-01-31 14:25:23 +0100

    HuB new portal prefab

---
+++ Assets/Scripts/OnHUBPortalEnter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnHUBPortalEnter : MonoBehaviour {
+
+    public int level;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.CompareTag("Player"))
+        {
+            Application.LoadLevel(level);
+        }
+    }
+}

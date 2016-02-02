850bdd10fb687166768c526c2ac8fc0b5a7d8038
Miroslava Jaresova <miroslava.jaresova@gmail.com>
2016-02-02 06:48:43 +0100

    visibility and hiding of hint images

---
+++ Assets/jumpHintScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class jumpHintScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Set Jump Hint Visible");
+            Image jump = GameObject.Find("jump").GetComponentInChildren<Image>();
+            jump.CrossFadeAlpha(100, 1, true);
+            //Image jump = GameObject.Find("CanvasHints").GetComponentInChildren<Image>();
+        }
+    }
+}
--- Assets/keyboardHintsScript.cs
+++ Assets/keyboardHintsScript.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class keyboardHintsScript : MonoBehaviour {
 
+    public Image move;
+    public Image jump;
+    public Image attack;
+    
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        jump.CrossFadeAlpha(0, 0, true);
+        attack.CrossFadeAlpha(0, 0, true);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            move.CrossFadeAlpha(0, 1, true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("SpaceBar pressed");
+            jump.CrossFadeAlpha(0, 1, true);
+            //jump.CrossFadeAlpha(0, 1, true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            attack.CrossFadeAlpha(0, 1, true);
+        }
+    }
+
+
 }

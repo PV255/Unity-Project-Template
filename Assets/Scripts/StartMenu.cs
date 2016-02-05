f1cd23f5024d4c97748970e264e691407f9e9d9c
jerggi <jerggi@gmail.com>
2016-02-05 21:20:16 +0100

    music playing is quite ok, not the best, but ok

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -38,6 +38,8 @@ public Canvas levelsCanvas;
 	public Button level2;
 	public Button level3;
 	public Button level4;
+
+    private bool music;
     // Use this for initialization
     void Start () {
         
@@ -82,6 +84,7 @@ lock1 = lock1.GetComponent<Text>();
         uiCanvas.enabled = false;
         gameOver.enabled = false; 
         levelsCanvas.enabled = false;
+        music = false;
     }
 
     public void ControlsPress()
@@ -147,9 +150,8 @@ Loading.SaveGame ();
     public void OkSoundDialog()
     {
         optionsCanvas.enabled = false;
-        if (musicToggle.isOn)
-            audioSource.mute = false;
-        else audioSource.mute = true;
+        if (musicToggle.isOn) music = true;
+        else music = false;
         MenuButtonsEnamble(true);
     }
 
@@ -186,6 +188,8 @@ public void ResetHighscore()
         snakeScript.setPause(snakeScript.isPaused());
         addPortalScript.setInMenu(false);
         addPortalScript.setPause(snakeScript.isPaused());
+        if (music) audioSource.mute = false;
+        else audioSource.mute = true;
     }
 
     public void GameOver()

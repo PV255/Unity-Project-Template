09975aef72646bd106ef2bda2ad6e03ccb2493b2
Ľubor Slušný <lubor.slusny@gmail.com>
2016-02-05 14:55:53 +0100

    added itemLife

---
+++ Assets/Scripts/ItemLife.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemLife : MonoBehaviour {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(false);
+            GameManager.Instance.AddLife();
+        }
+    }
+}
